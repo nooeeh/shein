060c92df462b475a369e598c32376efa6a2366c1
nooeeh <noejorq@gmail.com>
2026-02-05 13:14:30 +0100

    subida classes

--- src/main/java/org/palomafp/shein/modelo/App.java
+++ src/main/java/org/palomafp/shein/modelo/App.java
@@ -1,5 +1,7 @@
 package org.palomafp.shein.modelo;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Producto producto1 = new Producto(1, "M", "Rojo", "29.99", "Algodón", new Modelo(), new Categoria("Ropa", "Femenino"), List.of(Distribuidor.DISTRIBUIDOR1));
+        System.out.println(producto1);
+        Categoria categoria1 = new Categoria("Ropa", "Femenino");
+        System.out.println(categoria1);
+        
     }
 }
